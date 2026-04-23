fd0bd07f046c4eb39a592f56a5baeb5637aadbf9
Red1080 <redwankhan851@gmail.com>
2026-04-23 14:01:52 +0900

    Update main.py

--- IA2/main.py
+++ IA2/main.py
@@ -2,6 +2,10 @@ from Sensor import TemperatureSensor, LightSensor
 
 temp = TemperatureSensor("Temp1")
 light = LightSensor("Light")
+name = NameSensor("Name")
+humidity = HumiditySensor("Humidity")
 
 print(f"Temp: {temp.read()}")
 print(f"Light: {light.read()}")
+print(f"Name: {name.read()}")
+print(f"Humidity: {humidity.read()}")
