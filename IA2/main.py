df79a2248c500e04ed549d49ab9dd53010ce19c0
Red1080 <redwankhan851@gmail.com>
2026-06-04 15:27:21 +0900

    Add files via upload

--- IA2/main.py
+++ IA2/main.py
@@ -2,10 +2,6 @@ from Sensor import TemperatureSensor, LightSensor
 
 temp = TemperatureSensor("Temp1")
 light = LightSensor("Light")
-name = NameSensor("Name")
-humidity = HumiditySensor("Humidity")
 
 print(f"Temp: {temp.read()}")
 print(f"Light: {light.read()}")
-print(f"Name: {name.read()}")
-print(f"Humidity: {humidity.read()}")
